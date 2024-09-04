8f66e28efd1fd46714995e1a6ffa779dc30d67c0
FFFF-0000h <adebiyiitunuayo@gmail.com>
2024-09-04 11:44:13 +0100

    Polynomials: This program evaluates Polynomials using Horner's Rule given the value for x by the user

---
+++ Polynomial.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main (void)
+{
+    int x, Poly;
+    printf("Enter Value of x: ");
+    scanf("%d", &x);
+    printf("The value of the Polynomial is %d\n", Poly = ((((3 * x + 2) * x - 5) * x - 1) * x + 7) * x - 6); // Technique for evaluat­ing polynomials known as Horner’s Rule
+}
